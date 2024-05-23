e8b849d93336fc3c9ec88baafd7d40a72a396aa6
berri-puff <jennyzhang0207@gmail.com>
2024-05-23 15:38:52 +0100

    partial completion of the function statement

--- hangman.py
+++ hangman.py
@@ -10,7 +10,30 @@ def play(word):
     guessed = False
     guessed_letters = []
     guessed_ word = []
-    num_of_guesses = 6
+    tries = 6
+    print("Let's start a game of Hangman")
+    print(display_hangman(tries))
+    print(word_completion)
+    print("\n")
+    while guessed and tries > 0:
+        guess = input('Please enter a letter or word: ').lower()
+        if len(guess) == 1 and guess.isalpha():
+            if guess in guessed_letters:
+                print("You've already guessed this: ", guess)
+            elif guess not in word:
+                print("This word does not contain", guess)
+                tries -= 1
+                guessed_letters.append(guess)
+            else: 
+                print("Yay! ", guess, " is in the word!")
+                guessed_letters.append(guess)
+        elif len(guess) == len(word) and guess.isalpha():
+        
+        else: 
+            print("Not a valid guess, try again!")
+        print(display_hangman(tries))
+        print(word_completion)
+        print("\n")
 
 def display_hangman(tries):
     stages = [  # final state: head, torso, both arms, and both legs
