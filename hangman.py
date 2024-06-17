4b5d827e55005005c0eb55a4c6cebcacbcb841a3
berri-puff <jennyzhang0207@gmail.com>
2024-06-17 15:14:40 +0100

    completes the hangman py tutorial

--- hangman.py
+++ hangman.py
@@ -9,7 +9,7 @@ def play(word):
     word_completion = "_" * len(word)
     guessed = False
     guessed_letters = []
-    guessed_ word = []
+    guessed_word = []
     tries = 6
     print("Let's start a game of Hangman")
     print(display_hangman(tries))
@@ -27,13 +27,33 @@ def play(word):
             else: 
                 print("Yay! ", guess, " is in the word!")
                 guessed_letters.append(guess)
+                word_as_list = list(word_completion)
+                indices = [i for i, letter in enumerate(word) if letter == word]
+                for index in indices:
+                    word_as_list[index] == guess
+                word_completion = "".join(word_as_list)
+                if "_" not in word_completion:
+                    guessed = True
         elif len(guess) == len(word) and guess.isalpha():
-        
+            if guess == guessed_word:
+                print("You already guessed the word")
+                
+            elif guess != word:
+                print("That's not the word")
+                tries -= 1
+                guessed_word.append(guess)
+            else:
+                guessed = True 
+                word_completion = word
         else: 
             print("Not a valid guess, try again!")
         print(display_hangman(tries))
         print(word_completion)
         print("\n")
+    if guessed:
+        print("Yay! You guessed the word correctly")
+    else:
+        print("Sorry, you lose, the word was " + word )
 
 def display_hangman(tries):
     stages = [  # final state: head, torso, both arms, and both legs
@@ -108,3 +128,13 @@ def display_hangman(tries):
                 """
     ]
     return stages[tries]
+
+def main():
+    word = get_word
+    play(word)
+    while input("Play again? (Y/N)").upper()== "Y":
+        word = get_word
+        play(word)
+
+if __name__ == "__main__":
+    main()
